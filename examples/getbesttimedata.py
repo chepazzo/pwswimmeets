4a28dc0dd57ee20ceb71e9fd6b2e2b700c7b63d2
Mike Biancaniello <m.biancaniello@teamaol.com>
2013-07-11 17:02:37 -0400

    Added example html page to display data.

---
+++ examples/getbesttimedata.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import pwswimmeets
+import cgi
+import json
+
+form = cgi.FieldStorage()
+name = form.getvalue("name", None)
+
+print "Content-type: application/json"
+print
+print json.dumps(pwswimmeets.utils.get_best_times(name))
